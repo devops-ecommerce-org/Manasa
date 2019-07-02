09a95c9df338b74943d0372d10ad959c5f29e3a3
mgakkala <manasa.gakkala@gmail.com>
2019-07-02 17:05:14 +0000

    Committed from Unix Machine

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -2,3 +2,4 @@
 echo "Hello World"
 echo "Edited after committing - First Edit"
 echo "Created branches - made changes in only DEV branch to practise merging"
+echo "Created a repo on different machine(unix)and pulled from the GIT"
